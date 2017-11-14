fa46703f5523bc0d21f45cec9861bf77ca3e785e
Tobias <tobias@Tobiass-MacBook-Air.local>
2017-11-14 17:19:06 +0100

    First Commit

---
+++ includes/functions.inc.php
@@ -0,0 +1,55 @@
+<?php
+
+function myCalDaysInMonth($month, $year)
+{
+    return date('t', mktime(0, 0, 0, $month, 1, $year));
+}
+
+function buildMonthlyLabels($arrDate) {
+    $daysOfMonth = myCalDaysInMonth($arrDate['month'], $arrDate['year']);
+    $label = '';
+    for($i = 1; $i <= $daysOfMonth; $i++) {
+        $label .= "'" . $i . "', ";
+    }
+
+    return(substr($label, 0, -2));
+}
+
+function buildMyLabels($lCount) {
+    $label = '';
+    for($i = 1; $i <= $lCount; $i++) {
+        $label .= "'" . $i . "', ";
+    }
+
+    return(substr($label, 0, -2));
+}
+
+function getMonthyDistance($tDistance, $mDate) {
+
+}
+
+function buildTargetChart($tDistance, $tDate) {
+    if(!isset($tDate['month']) AND isset($tDate['year'])) {
+        $labelCount = date('z', mktime(0,0,0,12, 31, $tDate['year'])) + 1;
+        $dailyDistance = round($tDistance / $labelCount, 2);
+    } elseif (isset($tDate['month']) AND isset($tDate['year'])) {
+        $labelCount = myCalDaysInMonth($tDate['month'], $tDate['year']);
+        $mDistance = round($tDistance / 12, 2);
+        $dailyDistance = round($mDistance/ $labelCount, 2);
+    }
+
+
+    $dailyTotalDistance = $dailyDistance;
+    $targetCords = '';
+    $labels = '';
+
+    for($i = 1; $i <= $labelCount; $i++) {
+        $targetCords .= "{x: " . $i . ", y: " . $dailyTotalDistance . "}, \n";
+        $labels .= "'" . $i . "', ";
+        $dailyTotalDistance += $dailyDistance;
+    }
+
+    $myTargetData = array('label' => substr($labels, 0, -2), 'targetLine' => substr($targetCords,0, -2));
+
+    return ($myTargetData);
+}
---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tobias
+ * Date: 21.10.17
+ * Time: 10:05
+ */
+
+require_once 'vendor/autoload.php';
+require_once 'includes/functions.inc.php';
+
+use Symfony\Component\HttpFoundation\Request;
+$request = Request::createFromGlobals();
+$uri = $request->getPathInfo();
+
+$myTest = $request->query->all();
+
+
+switch ($myTest['p']) {
+    case 'tobi':
+        echo "Tobi - Learning to Code, ...<br />";
+        break;
+
+    case 'running':
+        echo "Stay healthy, ...<br />";
+        break;
+}
+
+
+print_r($myTest);
+
+echo "<br />" . $_GET['page'];
+
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array(
+    'cache' => false,
+    'debug' => true,
+));
+$twig->addExtension(new Twig_Extension_Debug());
+
+$pFFA = new adriangibbons\phpFITFileAnalysis('2183997374.fit');
+
+echo "<pre>";
+    print_r($pFFA->data_mesgs['session']);
+echo "</pre>";
+
+$startTime = date('d.M.Y h:m:s', $pFFA->data_mesgs['session']['start_time']);
+
+$tobi = $pFFA->data_mesgs['session'];
+$day = $pFFA->data_mesgs['session']['start_time'];
+$totalTime = $pFFA->data_mesgs['session']['total_timer_time'] / 60;
+
+
+$myDate = array('month' => date('m'), 'year' => date('Y'));
+$myDate = array('month' => '2', 'year' => '2017');
+
+$dayFormat = array('date' => date('Y-M-d', $day), 'day' => date('z', $day + 86400));
+
+$arrTester = array('month' => '2', 'year' => 2017);
+$dailyLabels = buildTargetChart('2000', $arrTester);
+
+//$tester = buildTargetChart('1200', $arrTester);
+
+echo $twig->render('chart.twig', array('totalTime' => $totalTime, 'startTime' => $startTime, 'tobi' => $tobi, 'datum' => $dayFormat, 'XLabels' => $dailyLabels['label'], 'targetLine' => $dailyLabels['targetLine']));
